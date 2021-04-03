7a8960e721c44143b82c82d3df82151b8fc00325
bomeng <bomeng@git.com>
2021-04-03 09:47:58 -0400

    WorkerManager

---
+++ EmployeeManagement/Manager_main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"WorkerManager.h"
+using namespace std;
+
+int main(){
+	WorkerManager mv;
+	mv.Show_Menu();
+	return 0;
+}
---
+++ EmployeeManagement/WorkerManager.cpp
@@ -0,0 +1,14 @@
+#include"WorkerManager.h"
+
+//WorkerManager::WorkerManager(){
+//}
+
+void WorkerManager::Show_Menu(){
+	cout << "************************************************" << endl;
+	cout << "**********************Welcome*******************" << endl;
+	cout << "0.exit 1.add 2.display 3.delete 4.medify 5.searh" << endl;
+	cout << "6.sort 7.distroy" << endl;
+}
+
+//WorkerManager::~WorkerManager(){
+//}
